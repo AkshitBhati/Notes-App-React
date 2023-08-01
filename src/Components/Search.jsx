5020ade1bb29ee5f51ab7e9b64998c0ca72082ae
AkshitBhati <akshitworks24@gmail.com>
2023-08-01 14:44:38 +0530

    update

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { BsSearch } from "react-icons/bs"
+import TextEditor from './TextEditor'
 
 const Search = () => {
   return (
+    <>
     <div className='flex justify-center align-center mt-5 '>
         <input type="text" className='w-1/2 p-1 outline-none' />
         <button className='border bg-textColor px-2 '><BsSearch /></button>
     </div>
+
+    <TextEditor />
+    </>
   )
 }
 
